6f50328fee5af8e95d3de07c671f51ae2e5d3c06
Justin Judice <142815014+JJudice51@users.noreply.github.com>
2025-02-07 16:29:30 -0600

    Update MenuBehavior.cs
    chore: need to get menuBehavior functioning.

--- Assets/_Project/UI/Scripts/MenuBehavior.cs
+++ Assets/_Project/UI/Scripts/MenuBehavior.cs
@@ -1,15 +1,40 @@
 using HurricaneVR.Framework.Components;
 using System.Collections;
 using System.Collections.Generic;
+using TMPro;
 using UnityEngine;
 using UnityEngine.Events;
 
+
+[RequireComponent(typeof(TMP_Text))]
 public class MenuBehavior : MonoBehaviour
 {
     [SerializeField]
     GameRoundSystem _roundSystem;
 
-    
+    private TMP_Text _buttonText;
+    private void Awake()
+    {
+        _buttonText = GetComponent<TMP_Text>();
+    }
+
+    private void FixedUpdate()
+    {
+        if()
+    }
+
+    private void OnEnable()
+    {
+        if(_buttonText)
+        {
+
+        }
+    }
+
+   // private IEnumerator 
+  //  {
+        
+   // }
 
 
 
