bc51f82d26c6039c040d3c38e08a4444c47d9ce1
Justin Judice <142815014+JJudice51@users.noreply.github.com>
2025-02-07 17:35:19 -0600

    Unable to get MenuBehavior to function as intended
    chore: need to add menus to came

--- Assets/_Project/UI/Scripts/MenuBehavior.cs
+++ Assets/_Project/UI/Scripts/MenuBehavior.cs
@@ -2,39 +2,20 @@ using HurricaneVR.Framework.Components;
 using System.Collections;
 using System.Collections.Generic;
 using TMPro;
+using Unity.VisualScripting;
 using UnityEngine;
 using UnityEngine.Events;
 
 
-[RequireComponent(typeof(TMP_Text))]
+
 public class MenuBehavior : MonoBehaviour
 {
     [SerializeField]
     GameRoundSystem _roundSystem;
-
-    private TMP_Text _buttonText;
-    private void Awake()
-    {
-        _buttonText = GetComponent<TMP_Text>();
-    }
-
-    private void FixedUpdate()
-    {
-        if()
-    }
-
-    private void OnEnable()
-    {
-        if(_buttonText)
-        {
-
-        }
-    }
-
-   // private IEnumerator 
-  //  {
-        
-   // }
+    [SerializeField]
+    HVRButton _button;
+   
+    
 
 
 
