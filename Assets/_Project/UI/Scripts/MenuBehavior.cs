c9d481815b07f2b6e60dff7695129ec5a1401359
Justin Judice <142815014+JJudice51@users.noreply.github.com>
2025-02-07 15:09:49 -0600

    moved around scripts added menu script that needs to be finished. Added unfinished buttons to scene
    chore: hands need implementation
    chore: need to implement game end
    chore: need to fix food in scene so that they are grabbable
    chore: need to move player character forward so that they are able to grab food.

---
+++ Assets/_Project/UI/Scripts/MenuBehavior.cs
@@ -0,0 +1,25 @@
+using HurricaneVR.Framework.Components;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+
+public class MenuBehavior : MonoBehaviour
+{
+    [SerializeField]
+    GameRoundSystem _roundSystem;
+
+    [SerializeField]
+    HVRButton _startButton;
+
+    [SerializeField]
+    HVRButton hVRButton;
+
+    private Coroutine _coroutine;
+
+
+
+
+
+
+}
