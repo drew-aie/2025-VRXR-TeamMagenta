c14eff727d6cbeb68853ef04e2d79f890d5dd003
Christian Deese <s238007@students.aie.edu>
2025-02-07 19:15:07 -0600

    fix: expanded customsocket class to prevent crashes

--- Assets/_Project/Core/Gameplay/FoodThrowables/Scripts/CustomSocket.cs
+++ Assets/_Project/Core/Gameplay/FoodThrowables/Scripts/CustomSocket.cs
@@ -7,16 +7,26 @@ public class CustomSocket : MonoBehaviour
 {
     public GameObject Product;
 
+    private List<GameObject> collisions;
 
     private void Awake()
     {
         Instantiate(Product, transform.position, transform.rotation);
     }
-
+    private void OnTriggerEnter(Collider other)
+    {
+        collisions.Add(other.gameObject);
+    }
     private void OnTriggerExit(Collider other)
     {
+        if(collisions.Contains(other.gameObject))
+            collisions.Remove(other.gameObject);
+
         if(other.GetComponent<ProjectileBehavior>())
-            Instantiate(Product, transform.position, transform.rotation);
+        {
+            if(collisions.Count < 1)
+                Instantiate(Product, transform.position, transform.rotation);
+        }
     }
 
 
